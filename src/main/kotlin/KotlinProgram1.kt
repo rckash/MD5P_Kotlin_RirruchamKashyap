bdffa01e210b2c0499f912cede0e97f4cedd572a
RC <rckashyapcpe@gmail.com>
2023-04-04 20:34:38 +0800

    modified KotlinProgram1.kt line 2

--- src/main/kotlin/KotlinProgram1.kt
+++ src/main/kotlin/KotlinProgram1.kt
@@ -1,4 +1,9 @@
 fun main() {
-    print("Hello World!")
+    println("Hello World!")
     print("Hi Universe!")
 }
+/*
+Turned print("Hi Universe!") into println("Hi Universe!")
+git add .\src\main\kotlin\KotlinProgram1.kt
+git commit -m"Modified KotlinProgram1.kt line 2"
+ */
