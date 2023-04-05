8b8814fec3846afd6c238e2bb559db03df293d8a
RC <rckashyapcpe@gmail.com>
2023-04-05 21:03:03 +0800

    created an application form program

---
+++ src/main/kotlin/KotlinActivity1.kt
@@ -0,0 +1,45 @@
+fun main() {
+    //Form Name
+    Println("Application for Employment")
+
+    //Personal Information
+    var positionApplied:String = "Mobile Developer"
+    var desiredSalary:Int = 100000
+    var dateAvailable:Int = 090923
+    var lastName:String = "Kashyap"
+    var firstName:String = "Rirrucham"
+    var middleName:String = "Casanova"
+    var address:String = "Calaca, Batangas"
+    var city:String = "Calaca"
+    var state:String = "Batangas"
+    var zip:Int = 4212
+    var homePhone:Int = 043696969
+    var cellPhone:Int = 09696969690
+    var email:String = "rirruchamkashyap@gmail.com"
+    var socialSecurityNum:Int = 696969696969
+    var isUSCitizen:Boolean = false
+    var isConvictedFelony = false
+    var isWillingToDrugScreen = true
+
+    //Education
+    var schoolName1:String = "Lyceum of the Philippines University--Batangas"
+    var location1:String = "Batangas"
+    var yearsAttended1:Int = 2
+    var degreeReceived1:String = "NA"
+    var major1:String = "Physical Therapy"
+    var schoolName2:String = "University of Batangas"
+    var location2:String = "Batangas"
+    var yearsAttended2:Int = 5
+    var degreeReceived2:String = "Bachelors"
+    var major2:String = "Computer Engineering"
+    var schoolName3:String = ""
+    var location3:String = ""
+    var yearsAttended3:Int = 5
+    var degreeReceived3:String = ""
+    var major3:String = ""
+
+
+
+
+
+}
