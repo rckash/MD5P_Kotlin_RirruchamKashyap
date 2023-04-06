a3b37d1ce7e9ad9467c01e0c52fc1e6cc1427533
RC <rckashyapcpe@gmail.com>
2023-04-06 13:37:47 +0800

    added variables for employment,references, acknoledgement and authorization, and signature portions

--- src/main/kotlin/KotlinActivity1.kt
+++ src/main/kotlin/KotlinActivity1.kt
@@ -1,11 +1,11 @@
 fun main() {
     //Form Name
-    Println("Application for Employment")
+    println("Application for Employment")
 
     //Personal Information
     var positionApplied:String = "Mobile Developer"
     var desiredSalary:Int = 100000
-    var dateAvailable:Int = 090923
+    var dateAvailable:Int = 90923
     var lastName:String = "Kashyap"
     var firstName:String = "Rirrucham"
     var middleName:String = "Casanova"
@@ -13,10 +13,10 @@ fun main() {
     var city:String = "Calaca"
     var state:String = "Batangas"
     var zip:Int = 4212
-    var homePhone:Int = 043696969
-    var cellPhone:Int = 09696969690
+    var homePhone:Int = 43696969
+    var cellPhone:Long = 9696969690
     var email:String = "rirruchamkashyap@gmail.com"
-    var socialSecurityNum:Int = 696969696969
+    var socialSecurityNum:Long = 696969696969
     var isUSCitizen:Boolean = false
     var isConvictedFelony = false
     var isWillingToDrugScreen = true
@@ -24,19 +24,62 @@ fun main() {
     //Education
     var schoolName1:String = "Lyceum of the Philippines University--Batangas"
     var location1:String = "Batangas"
-    var yearsAttended1:Int = 2
+    var yearsAttended1:String = "2015-2017"
     var degreeReceived1:String = "NA"
     var major1:String = "Physical Therapy"
     var schoolName2:String = "University of Batangas"
     var location2:String = "Batangas"
-    var yearsAttended2:Int = 5
+    var yearsAttended2:String = "2017-2022"
     var degreeReceived2:String = "Bachelors"
     var major2:String = "Computer Engineering"
     var schoolName3:String = ""
     var location3:String = ""
-    var yearsAttended3:Int = 5
+    var yearsAttended3:String = ""
     var degreeReceived3:String = ""
     var major3:String = ""
+    var otherEducation:String = "TESDA NCII - Computer Systems Servicing"
+
+    //Employment
+    var employer:String = "Rowan Atkinson"
+    var datesEmployedStart:Int = 60122
+    var datesEmployedEnd:Int = 40622
+    var workPhone:Long = 9123456789
+    var payRate:Int = 20000
+    var addressEmployment:String = "BGC, Taguig"
+    var cityEmployment:String = "Taguig"
+    var stateEmployment:String = "Metro Manila"
+    var zipEmployment:Int = 1637
+    var positionEmployment:String = "Production Engineer"
+    var dutiesPerformed:String = "Events handling, documentation, collaboration"
+    var supervisorsNameTitle:String = "Tim Agnacio, Team Leader"
+    var reasonForLeaving:String = "Immigration"
+    var isContactLastEmployer:Boolean = true
+
+    //References
+    var referenceName1:String = "Jonathan Maligaya"
+    var referenceTitle1:String = "Senior UI/UX"
+    var referenceCompany1:String = "Logi4,Inc."
+    var referencePhoneNum1:String = "99258789654"
+    var referenceName2:String = "Therese Domingo"
+    var referenceTitle2:String = "Taho Vendor"
+    var referenceCompany2:String = "Taho R Us"
+    var referencePhoneNum2:String = "99258789333"
+    var referenceName3:String = "Anthony Quezon"
+    var referenceTitle3:String = "Head Teacher III"
+    var referenceCompany3:String = "University of Calaca"
+    var referencePhoneNum3:String = "99258555888"
+
+    //Acknowledgement and Authorization
+    var isCertifyTrue:Boolean = true
+    var isAuthorizeInvestigation = true
+    var isMisleadingDischarge = true
+
+    //Signature and Date
+    var applicantSignature:String = "rkashyap"
+    var dateApplied:Int = 40623
+
+
+
 
 
 
