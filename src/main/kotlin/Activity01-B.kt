77dce00087c296cce528548b11bf27dc4edd5f68
RC <rckashyapcpe@gmail.com>
2023-04-11 20:41:26 +0800

    created a program for the automation of a school library

---
+++ src/main/kotlin/Activity01-B.kt
@@ -0,0 +1,38 @@
+fun main() {
+    /*You are tasked to automate a school’s library.
+    Identify the items that can be borrowed in a Library.
+    After listing the different items, identify the characteristics of the items.
+    Use the proper data types.*/
+    var isBook:Boolean
+    var isMagazine:Boolean
+    var isNewspaper:Boolean
+    var isMap:Boolean
+    var isCD:Boolean
+    var isCassette:Boolean
+    var isTool:Boolean
+    var isMisc:Boolean
+    var bookName:String
+    var bookNum:Int
+    var bookType:String
+    var magazineName:String
+    var magazineNum:Int
+    var magazineType:String
+    var newspaperName:String
+    var newspaperNum:Int
+    var newspaperType:String
+    var mapName:String
+    var mapNum:Int
+    var mapType:String
+    var cdName:String
+    var cdNum:Int
+    var cdType:String
+    var cassetteName:String
+    var cassetteNum:Int
+    var cassetteType:String
+    var toolName:String
+    var toolNum:Int
+    var toolType:String
+    var miscName:String
+    var miscNum:Int
+    var miscType:String
+}
