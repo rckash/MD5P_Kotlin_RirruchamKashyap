8172b7f2c03d2c4878cf5d613f9b71f1b833e7a1
RC <rckashyapcpe@gmail.com>
2023-04-11 20:27:01 +0800

    created a program for students monitoring of a course

---
+++ src/main/kotlin/Activity01-A.kt
@@ -0,0 +1,33 @@
+fun main() {
+    /*You are tasked to create a directory of the students taking this course.
+    The data will be used to send updates regarding the classes.
+    There is also a need to know the progress of the students during the progress of this course.
+    List down all data that can be used to implement this. Use the proper data types.*/
+    var lastName:String
+    var firstName:String
+    var homeAddress:String
+    var phoneNum:Int
+    var section:Char
+    val activity1Score:Int
+    val activity2Score:Int
+    val Absences1stQtr:Int
+    val prelimExamScore: Int
+    val activity3Score:Int
+    val activity4Score:Int
+    val Absences2ndQtr:Int
+    val midtermExamScore:Int
+    var midtermGrade:Float
+    val activity5Score:Int
+    val activity6Score:Int
+    val Absences3rdQtr:Int
+    val semifinalExamScore: Int
+    val activity7Score:Int
+    val activity8Score:Int
+    val Absences4thQtr:Int
+    var isDroppedFromClass:Boolean
+    val AbsencesTotal:Int
+    var finalExamScore:Int
+    var finalProjectScore:Int
+    var finalGrade:Float
+    var isPassedCourse:Boolean
+}
