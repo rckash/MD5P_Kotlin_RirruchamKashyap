737aba94fe7057aafedc3f0b7268af28461385b0
RC <rckashyapcpe@gmail.com>
2023-04-11 21:01:32 +0800

    created a program for the automation of an inventory system for a grocery store

---
+++ src/main/kotlin/Activity01-C.kt
@@ -0,0 +1,27 @@
+fun main() {
+    /*You are tasked to automate an inventory system for a grocery.
+    Identify the items that can be bought in a grocery store.
+    After listing the different items, identify the characteristics of the items.
+    You are also tasked to group the items in categories to help manage the grocery.  Use the proper data types.*/
+    var itemName:String
+    var isFood:Boolean
+    var foodType:String
+    var isDrink:Boolean
+    var drinkType:String
+    var isUtensil:Boolean
+    var utensilType:String
+    var isCleaningProduct:Boolean
+    var cleaningProductType:String
+    var isTool:Boolean
+    var toolType:String
+    var isMisc:Boolean
+    var miscType:String
+    var productBarCode:Int
+    var expirationDate:String
+    var price:Float
+    var numInStock:Int
+    var totalNumOfStock:Int
+    var expectedNextRestock:String
+    var isSale:Boolean
+    var discount:Float
+}
