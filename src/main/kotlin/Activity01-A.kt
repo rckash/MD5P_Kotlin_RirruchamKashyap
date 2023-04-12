7ab36f77554932d00255234c80e4abc7237f112c
RC <rckashyapcpe@gmail.com>
2023-04-12 19:20:31 +0800

    added var and val default values

--- src/main/kotlin/Activity01-A.kt
+++ src/main/kotlin/Activity01-A.kt
@@ -3,31 +3,33 @@ fun main() {
     The data will be used to send updates regarding the classes.
     There is also a need to know the progress of the students during the progress of this course.
     List down all data that can be used to implement this. Use the proper data types.*/
-    var lastName:String
-    var firstName:String
-    var homeAddress:String
-    var phoneNum:Int
-    var section:Char
-    val activity1Score:Int
-    val activity2Score:Int
-    val Absences1stQtr:Int
-    val prelimExamScore: Int
-    val activity3Score:Int
-    val activity4Score:Int
-    val Absences2ndQtr:Int
-    val midtermExamScore:Int
-    var midtermGrade:Float
-    val activity5Score:Int
-    val activity6Score:Int
-    val Absences3rdQtr:Int
-    val semifinalExamScore: Int
-    val activity7Score:Int
-    val activity8Score:Int
-    val Absences4thQtr:Int
-    var isDroppedFromClass:Boolean
-    val AbsencesTotal:Int
-    var finalExamScore:Int
-    var finalProjectScore:Int
-    var finalGrade:Float
-    var isPassedCourse:Boolean
+    var lastName:String = "Kashyap"
+    var firstName:String = "RC"
+    var homeAddress:String = "Calaca, Batangas"
+    var phoneNum:Long = 9123456789
+    var section:Char = 'A'
+    val activity1Score:Int = 79
+    val activity2Score:Int = 78
+    val Absences1stQtr:Int = 0
+    val prelimExamScore: Int = 99
+    val activity3Score:Int = 69
+    val activity4Score:Int = 44
+    val Absences2ndQtr:Int = 2
+    val midtermExamScore:Int = 96
+    var midtermGrade:Float = 85.67f
+    val activity5Score:Int = 46
+    val activity6Score:Int = 99
+    val Absences3rdQtr:Int = 1
+    val semifinalExamScore: Int = 98
+    val activity7Score:Int = 88
+    val activity8Score:Int = 76
+    val Absences4thQtr:Int = 0
+    var isDroppedFromClass:Boolean = false
+    val AbsencesTotal:Int = 1
+    var finalExamScore:Int = 99
+    var finalProjectScore:Int = 99
+    var finalGrade:Float = 91.77f
+    var isPassedCourse:Boolean = true
+
+
 }
