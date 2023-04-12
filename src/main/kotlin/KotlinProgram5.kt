e471dabd50cd52e9da55882499b284f9a03b94d2
RC <rckashyapcpe@gmail.com>
2023-04-12 20:49:45 +0800

    created a program demonstrating some logical operators

---
+++ src/main/kotlin/KotlinProgram5.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var lessThan = 5 < 2 //false
+    var greaterThan = 10 > 2 //true
+    var greaterThanOrEqualTo = 10 >= 4 //true
+    var lessThanOrEqualTo = 5 <= 2 //false
+    var equalTo = 1 == 1 //true
+    var notEqualTo = 1 != 1 //false
+
+    println(lessThan)
+    println(greaterThan)
+    println(greaterThanOrEqualTo)
+    println(lessThanOrEqualTo)
+    println(equalTo)
+    println(notEqualTo)
+}
