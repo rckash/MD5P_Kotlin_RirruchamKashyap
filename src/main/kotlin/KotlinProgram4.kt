f1bb0211c1beb6d5cc4cce46b9a4ba8d9f311ec4
RC <rckashyapcpe@gmail.com>
2023-04-12 20:30:19 +0800

    created a program for arithmetic operators

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,16 @@
+fun main() {
+    // arithmetic operators
+    println("This is an example of addition: ${2+5}")
+    // store in a variable
+    var sum : Int = 5 + 8
+    var difference : Int = 10 - 8
+    var product : Int = 5 * 8
+    var quotient : Int = 100 / 25
+    var modulo : Int = 5 % 2
+
+    println("The sum is $sum")
+    println("The difference is $difference")
+    println("The product is $product")
+    println("The quotient is $quotient")
+    println("The modulo is $modulo")
+}
