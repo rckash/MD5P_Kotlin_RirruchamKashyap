4f439cb02c6ebac2d664db3636bd59457b9d824b
RC <rckashyapcpe@gmail.com>
2023-04-12 19:21:01 +0800

    added var and val default values

--- src/main/kotlin/Activity01-B.kt
+++ src/main/kotlin/Activity01-B.kt
@@ -3,36 +3,36 @@ fun main() {
     Identify the items that can be borrowed in a Library.
     After listing the different items, identify the characteristics of the items.
     Use the proper data types.*/
-    var isBook:Boolean
-    var isMagazine:Boolean
-    var isNewspaper:Boolean
-    var isMap:Boolean
-    var isCD:Boolean
-    var isCassette:Boolean
-    var isTool:Boolean
-    var isMisc:Boolean
-    val bookName:String
-    var bookNum:Int
-    var bookType:String
-    val magazineName:String
-    var magazineNum:Int
-    var magazineType:String
-    val newspaperName:String
-    var newspaperNum:Int
-    var newspaperType:String
-    val mapName:String
-    var mapNum:Int
-    var mapType:String
-    val cdName:String
-    var cdNum:Int
-    var cdType:String
-    val cassetteName:String
-    var cassetteNum:Int
-    var cassetteType:String
-    val toolName:String
-    var toolNum:Int
-    var toolType:String
-    val miscName:String
-    var miscNum:Int
-    var miscType:String
+    var isBook:Boolean = false
+    var isMagazine:Boolean = false
+    var isNewspaper:Boolean = false
+    var isMap:Boolean = false
+    var isCD:Boolean = false
+    var isCassette:Boolean = false
+    var isTool:Boolean = false
+    var isMisc:Boolean = false
+    val bookName:String = "Book of Love"
+    var bookNum:Int = 69
+    var bookType:String = "Coloring"
+    val magazineName:String = "Magasin"
+    var magazineNum:Int = 33333
+    var magazineType:String = "Kids"
+    val newspaperName:String = "La Solidaridad"
+    var newspaperNum:Int = 2323234
+    var newspaperType:String = "Scientific"
+    val mapName:String = "Dora's"
+    var mapNum:Int = 998989
+    var mapType:String = "World"
+    val cdName:String = "How to Dance"
+    var cdNum:Int = 3444666
+    var cdType:String = "Instructional"
+    val cassetteName:String = "Jon Bovi Greatest Hits"
+    var cassetteNum:Int = 4324343
+    var cassetteType:String = "Music"
+    val toolName:String = "Stapler"
+    var toolNum:Int = 43434
+    var toolType:String = "Office"
+    val miscName:String = "Globe"
+    var miscNum:Int = 112323
+    var miscType:String = "Learning Material"
 }
