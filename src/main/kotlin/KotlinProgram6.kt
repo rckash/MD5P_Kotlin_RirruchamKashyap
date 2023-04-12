786b591a55c44f429a49c9f41029214793dc4845
RC <rckashyapcpe@gmail.com>
2023-04-12 20:55:28 +0800

    created a program demonstrating logical operators

---
+++ src/main/kotlin/KotlinProgram6.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // logical operators
+    var andOperator = (6 > 1) && (5 > 2) // will only be true if both statements are true
+    var orOperator = (1 > 5) || (6 > 3) // will only be false if both statements are false
+    var notOperator = !(5 == 5) // inverts the result
+
+    println(andOperator)
+    println(orOperator)
+    println(notOperator)
+
+}
