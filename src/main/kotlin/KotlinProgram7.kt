d02153a5796c36b85abe8351afca4f5865818832
RC <rckashyapcpe@gmail.com>
2023-04-13 19:54:09 +0800

    created a program demonstrating increment and decrement functions

---
+++ src/main/kotlin/KotlinProgram7.kt
@@ -0,0 +1,17 @@
+fun main() {
+    // increment ++ and decrement -- operators
+    var x : Int = 1
+    println(x++) // post-increment // displays 1
+    println(x)
+    println(++x) // pre-increment
+    println(x)
+
+    var y : Int = 5
+    println(--y) // displays 4
+    println(y--) // displays 4
+    println(y) // displays 3
+
+    var z : Int = 1
+    z-=5
+    println(z)
+}
