4c7f260ead381cd5dae957f6dd04d396885f634b
RC <rckashyapcpe@gmail.com>
2023-04-14 20:16:55 +0800

    changed some vars into input vars

--- src/main/kotlin/PracticeActivity3.kt
+++ src/main/kotlin/PracticeActivity3.kt
@@ -6,9 +6,20 @@ fun main() {
     var priceDay3 : Float = 38.97f
     var averagePrice : Float = 0f
 
+    // input
+    println("Enter item name: ")
+    commodityItem = readln()
+    println("Price in Day 1: ")
+    priceDay1 = readln().toFloat()
+    println("Price in Day 2: ")
+    priceDay2 = readln().toFloat()
+    println("Price in Day 3: ")
+    priceDay3 = readln().toFloat()
+
+
     // process
     averagePrice = (priceDay1 + priceDay2 + priceDay3) / 3
 
     // output
-    println("The average price of $commodityItem is $averagePrice per gallon" )
+    println("The average price of $commodityItem is ${String.format("%.2f",averagePrice)} per gallon" )
 }
