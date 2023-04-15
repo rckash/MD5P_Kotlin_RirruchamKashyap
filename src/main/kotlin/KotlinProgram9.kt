bea8ebebb41abf6cbd40a2b513bb976fb534ace3
RC <rckashyapcpe@gmail.com>
2023-04-15 10:06:53 +0800

    added some comments

--- src/main/kotlin/KotlinProgram9.kt
+++ src/main/kotlin/KotlinProgram9.kt
@@ -1,11 +1,14 @@
 fun main() {
+    // if statement
     // variables
     var age : Int = 0
     // < > <= >= == !=
 
+    // input
     println("Please Enter your age: ")
     age = readln().toInt()
 
+    // process + output
     if(age >= 18) {
         println("Your age is $age")
         println("You are qualified to vote!")
