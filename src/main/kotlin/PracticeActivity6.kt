f83497ac133cf5a7ff3aaf7c739fd71f2c58ec45
RC <rckashyapcpe@gmail.com>
2023-04-15 11:18:35 +0800

    created a program for determining positive, negative, zero numbers

---
+++ src/main/kotlin/PracticeActivity6.kt
@@ -0,0 +1,19 @@
+fun main() {
+    // variable
+    var num : Int = 0
+
+    // input
+    println("Enter a number: ")
+    num = readln().toInt()
+
+    //process + output
+    if (num > 0) {
+        println("The number is positive.")
+    }
+    else if (num < 0) {
+        println("The number is negative.")
+    }
+    else {
+        println("The number is zero.")
+    }
+}
