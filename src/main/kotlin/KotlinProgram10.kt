9dcdec29fc991a20f369fe8e7ea83ca03adbc488
RC <rckashyapcpe@gmail.com>
2023-04-15 10:12:24 +0800

    created a program demonstrating if-else statement

---
+++ src/main/kotlin/KotlinProgram10.kt
@@ -0,0 +1,20 @@
+fun main() {
+    // if statement
+    // variables
+    var age : Int = 0
+    // < > <= >= == !=
+
+    // input
+    println("Please Enter your age: ")
+    age = readln().toInt()
+
+    // process + output
+    if(age >= 18) {
+        println("Your age is $age")
+        println("You are qualified to vote!")
+    }else {
+        println("Your age is $age")
+        println("You are not qualified to vote!")
+    }
+    println("This will always execute")
+}
