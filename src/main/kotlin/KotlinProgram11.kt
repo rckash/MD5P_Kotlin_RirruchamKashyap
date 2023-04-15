76251cbf5efab74add86e19ec5fc0e7e6bb0f4fe
RC <rckashyapcpe@gmail.com>
2023-04-15 10:33:04 +0800

    created a program demonstrating if else if statement

---
+++ src/main/kotlin/KotlinProgram11.kt
@@ -0,0 +1,24 @@
+fun main() {
+    //if else if statement
+    // age 0-12 -> Kids
+    // age 13-18 -> Teenagers
+    // age 19-59 -> Adult
+    // age 60 and above -> Senior Citizen
+    // less than 0 -> Invalid age
+    var age : Int = 0
+    // < > <= >= == !=
+    println("Please Enter your age: ")
+    age = readln().toInt()
+
+    if (age >= 60) {
+        println("Senior Citizen")
+    }else if(age >= 19) {
+        println("Adults")
+    }else if(age >= 13) {
+        println("Teenagers")
+    }else if(age >= 0) {
+        println("Kids")
+    }else {
+        println("Invalid age")
+    }
+}
