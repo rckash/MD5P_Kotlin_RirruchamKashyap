bb2670f39c9e902ee8e6fbef706cf9ff56ea808c
RC <rckashyapcpe@gmail.com>
2023-04-15 15:45:07 +0800

    fixed price range value

--- src/main/kotlin/PracticeActivity10.kt
+++ src/main/kotlin/PracticeActivity10.kt
@@ -16,11 +16,11 @@ fun main() {
         discountPrice = salesPrice * 0.2f
     } else if (salesPrice >= 50001) {
         discountPrice = salesPrice * 0.15f
-    } else if (salesPrice >= 50001) {
+    } else if (salesPrice >= 20001) {
         discountPrice = salesPrice * 0.1f
-    } else if (salesPrice >= 50001) {
+    } else if (salesPrice >= 10001) {
         discountPrice = salesPrice * 0.05f
-    } else if (salesPrice >= 50001) {
+    } else if (salesPrice >= 0) {
         discountPrice = salesPrice
     } else {
         println("Invalid sales price: Please enter a positive number.")
@@ -32,5 +32,4 @@ fun main() {
     println("Price of $productName is ${String.format("%.2f", salesPrice)}")
     println("Discount is ${String.format("%.2f", discountPrice)}")
     println("Net price is ${String.format("%.2f", netPrice)}")
-
 }
