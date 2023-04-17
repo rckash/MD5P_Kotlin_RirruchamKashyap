fec26a810c03ae914307c158da8cfe254bd89ac9
RC <rckashyapcpe@gmail.com>
2023-04-17 20:07:38 +0800

    separated process and output as it is best practice

--- src/main/kotlin/PracticeActivity15.kt
+++ src/main/kotlin/PracticeActivity15.kt
@@ -3,6 +3,8 @@ fun main() {
     var num1 : Int = 0
     var num2 : Int = 0
     var num3 : Int = 0
+    var leftNum = 0
+    var rightNum = 0
 
 
     // input
@@ -13,8 +15,12 @@ fun main() {
     println("Enter the third number: ")
     num3 = readln().toInt()
 
-    // process + output
-    if ((num1 * num1 + num2 * num2) == (num3 * num3)){
+    // process
+    leftNum = num1 * num1 + num2 * num2
+    rightNum = num3 * num3
+
+    // output
+    if (leftNum == rightNum){
         println("These numbers form a Pythagorean triple.")
     } else {
         println("These numbers do not form a Pythagorean triple.")
