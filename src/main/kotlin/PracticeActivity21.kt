9fc070b09a99ed0c8f8e81d7855d06a4d4f9d22c
RC <rckashyapcpe@gmail.com>
2023-04-20 20:18:05 +0800

    created a program that prints out a series of numbers

---
+++ src/main/kotlin/PracticeActivity21.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // outerloop
+    var ctr1 = 1
+    while (ctr1 <= 5) {
+        // innerloop
+        var ctr2 = 1
+        while (ctr2 <= ctr1) {
+            print("$ctr2 ")
+            ctr2++
+        }
+        println()
+        ctr1++
+    }
+}
