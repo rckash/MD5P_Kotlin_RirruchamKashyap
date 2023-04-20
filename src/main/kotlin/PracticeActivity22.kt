1aaec05fb77b1bf93c51a5f0bbbc7d4ed0a6c721
RC <rckashyapcpe@gmail.com>
2023-04-20 20:20:45 +0800

    created a program that prints out a series of pattern

---
+++ src/main/kotlin/PracticeActivity22.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // outerloop
+    var ctr1 = 1
+    while (ctr1 <= 5) {
+        // innerloop
+        var ctr2 = 5
+        while (ctr2 >= ctr1) {
+            print("*")
+            ctr2--
+        }
+        println()
+        ctr1++
+    }
+}
