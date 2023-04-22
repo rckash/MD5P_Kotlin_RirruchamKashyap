c0d0a0f0dd8539518762113f8cc8f76c3eedc63b
RC <rckashyapcpe@gmail.com>
2023-04-22 13:50:16 +0800

    created a program showing number square and cube

---
+++ src/main/kotlin/practiceactivities/PracticeActivity24.kt
@@ -0,0 +1,32 @@
+package practiceactivities
+
+fun main() {
+    //variables declaration
+    var num = 2
+    var numTotal = 0
+    var square = 0
+    var squareTotal = 0
+    var cube = 0
+    var cubeTotal = 0
+    var ctr1 = 1
+
+    while (ctr1 < 6) {
+        // calculation for rows
+        num = ctr1 * 2
+        square = num * num
+        cube = num * num * num
+
+        // calculation for totals
+        numTotal += num
+        squareTotal += square
+        cubeTotal += cube
+
+        // num output
+        println("$num\t$square\t$cube")
+
+        ctr1++
+    }
+
+    // total output
+    println("$numTotal\t$squareTotal\t$cubeTotal")
+}
