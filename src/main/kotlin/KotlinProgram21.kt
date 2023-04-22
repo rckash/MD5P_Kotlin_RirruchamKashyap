64b3dacf97e13adeda156e278d588f892a6c7b28
RC <rckashyapcpe@gmail.com>
2023-04-22 09:54:43 +0800

    created a program demonstrating arrayOf

---
+++ src/main/kotlin/KotlinProgram21.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var names = arrayOf("paul","john","ringo","george")
+    println("array size is ${names.size}")
+
+    var ctr : Int = 0
+    while (ctr < names.size) {
+        println(names[ctr])
+        ctr++
+    }
+}
