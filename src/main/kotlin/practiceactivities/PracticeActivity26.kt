ecc3a70d2b317ebe0c2fe73e11c4003fafe4fdc7
RC <rckashyapcpe@gmail.com>
2023-04-23 18:58:42 +0800

    used 2 loops instead

--- src/main/kotlin/practiceactivities/PracticeActivity26.kt
+++ src/main/kotlin/practiceactivities/PracticeActivity26.kt
@@ -2,24 +2,24 @@ package practiceactivities
 
 fun main() {
     // variables declaration
+    var columns = 5
+    var num = 25
+    var numGenY = num
+    var numGenX = num
+
+    // outer loop
     var ctr1 = 0
-    var numCol1 = 25
-    var numCol2 = numCol1 - 5
-    var numCol3 = numCol2 - 5
-    var numCol4 = numCol3 - 5
-    var numCol5 = numCol4 - 5
-    var num = arrayOf(25, 20, 15, 10, 5)
+    for (i in 1..columns) {
 
+        // inner loop
+        for (i in 1..columns) {
+            print("$numGenX\t")
+            numGenX -= 5
+        }
 
-
-    // process + output
-    while (ctr1 < 5) {
-        println("$numCol1\t$numCol2\t$numCol3\t$numCol4\t$numCol5")
-        numCol1 -= 1
-        numCol2 -= 1
-        numCol3 -= 1
-        numCol4 -= 1
-        numCol5 -= 1
-        ctr1++
+        // setting value for column
+        numGenY -= 1
+        numGenX = numGenY
+        println()
     }
 }
