c6cf09c6f8e6acf7d4cd39c932469af81dd43179
RC <rckashyapcpe@gmail.com>
2023-04-23 14:33:36 +0800

    added comments

--- src/main/kotlin/practiceactivities/PracticeActivity25.kt
+++ src/main/kotlin/practiceactivities/PracticeActivity25.kt
@@ -1,13 +1,16 @@
 package practiceactivities
 
 fun main() {
+    // input
     var baseSize = 0
     println("Enter Base Size of Dollar Triangle: ")
     baseSize = readln().toInt()
 
+    // outer loop
     var space = baseSize - 1
     for(i in 1 .. baseSize){
         var toPrint = ""
+        // inner loop for string formation
         for(i in 1 .. baseSize){
             if(i > space){
                 toPrint += "$"
@@ -15,7 +18,7 @@ fun main() {
                 toPrint += " "
             }
         }
-        println(toPrint)
+        println(toPrint)    // prints string
         space -= 1
     }
 }
