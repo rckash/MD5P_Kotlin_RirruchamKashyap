fc5a5e0cec236d01c9abbe8fde709010cd74f3b6
RC <rckashyapcpe@gmail.com>
2023-04-26 19:56:51 +0800

    created a program demonstrating a matrix

---
+++ src/main/kotlin/kotlinprograms/KotlinProgram29.kt
@@ -0,0 +1,24 @@
+package kotlinprograms
+
+fun main() {
+    val matrix = listOf(
+        //      0   1   2
+        listOf('a','b','c'), // 0
+        listOf('d','e','f'), // 1
+        listOf('g','h','i'), // 2
+        listOf('j','k','l'), // 3
+    )
+    println(matrix.size) // get the number of rows
+
+    var row = 0
+    // 0-3
+    while (row < matrix.size) {
+        var column = 0
+        while (column < matrix[column].size) {
+            println("${matrix[row][column]}")
+            column++
+        }
+        row++
+        println()
+    }
+}
