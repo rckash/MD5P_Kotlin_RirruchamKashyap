340a807ce3317c3bebeaa40ed93c6cac3daa521b
RC <rckashyapcpe@gmail.com>
2023-04-27 19:45:02 +0800

    improved code cleanliness

--- src/main/kotlin/practiceactivities/PracticeActivity27.kt
+++ src/main/kotlin/practiceactivities/PracticeActivity27.kt
@@ -1,6 +1,21 @@
 package practiceactivities
 
 fun main() {
+//    Create a program that displays the matrix of a number entered by the user. If a user enters 4, it
+//    will generate 16 numbers; if 3, it will generate 9 numbers.
+//    Arrange the series of numbers in the order just like the sample output.
+//    Note: Maximum 3 println() or print() should be used in the solution. Use two looping statements
+//    only.Create a program that displays the matrix of a number entered by the user. If a user enters 4, it
+//    will generate 16 numbers; if 3, it will generate 9 numbers.
+//    Arrange the series of numbers in the order just like the sample output.
+//    Note: Maximum 3 println() or print() should be used in the solution. Use two looping statements
+//    only.
+
+    // 16   15  14  13
+    // 9    10  11  12
+    // 8    7   6   5
+    // 1    2   3   4
+
     // input
     var num = 0
     println("Enter matrix to generate (n by n):")
@@ -9,8 +24,6 @@ fun main() {
     // variables declaration for the two loops
     var ctr1 = 0
     var matNum = num * num
-    var matNumX = matNum
-    var matNumY = matNum
 
     // outer loop
     for (i in 1..num) {
@@ -21,15 +34,15 @@ fun main() {
 
             // inner loop for generation of numbers in the matrix
             if (itCheck == 0) {
-                matNumX++
-                print("$matNumX\t")
+                matNum++
+                print("$matNum\t")
             } else {
-                print("$matNumX\t")
-                matNumX--
+                print("$matNum\t")
+                matNum--
             }
 
         }
         println()
-        matNumX -= num
+        matNum -= num
     }
     }
