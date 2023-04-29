98b742c0e1b2bd9a39c26f87e6aa138df39ebe6f
RC <rckashyapcpe@gmail.com>
2023-04-29 10:30:26 +0800

    created a program demonstrating return for functions

---
+++ src/main/kotlin/kotlinprograms/KotlinProgram32.kt
@@ -0,0 +1,17 @@
+package kotlinprograms
+
+fun main() {
+    var number = returnDigit()
+    println(number)
+    var name = returnName()
+    println(name)
+}
+
+fun returnDigit(): Int {
+    println("Hello this is returnDigitFunction")
+    return 29
+}
+
+fun returnName(): String {
+    return "John"
+}
