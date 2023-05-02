774be97d3151af39c7ffe7db8dfa26b98994c783
RC <rckashyapcpe@gmail.com>
2023-05-02 14:18:51 +0800

    created a program that replaces spaces with underscores

---
+++ src/main/kotlin/practiceactivities/PracticeActivity41.kt
@@ -0,0 +1,36 @@
+package practiceactivities
+
+//Write a function called replaceSpaces() that takes in a string and replaces all spaces with underscores.
+//Sample Input/Output
+//
+//Enter a string: Hello world!
+//String with spaces replaced: Hello_world!
+
+
+fun main() {
+    //input
+    print("Enter String: ")
+    var string = readln()
+
+    //return value storage
+    var noSpaces = replaceSpaces(string)
+
+    //output
+    println("String with spaces replaced: ")
+    println(noSpaces)
+}
+
+fun replaceSpaces(string: String): String {
+    var stringIndexSize = string.length - 1
+    var noSpaces = ""
+
+    for (i in 0..stringIndexSize) {
+        if (string[i] == ' ') {
+            noSpaces += '_'
+        } else {
+            noSpaces += string[i]
+        }
+    }
+
+    return noSpaces
+}
