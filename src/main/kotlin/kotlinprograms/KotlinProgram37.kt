d2117ce9170029f021c97563ae147c1d9c623cd7
RC <rckashyapcpe@gmail.com>
2023-05-02 20:21:12 +0800

    created a program demonstrating finally for exception handling

---
+++ src/main/kotlin/kotlinprograms/KotlinProgram37.kt
@@ -0,0 +1,24 @@
+package kotlinprograms
+
+fun main() {
+    var number1 : Int = 0
+    var number2 : Int = 0
+    //Exception handling - try and catch
+    try {
+        println("***********************")
+        println("Simple Calculator")
+        println("***********************")
+        println("Enter number 1: ")
+        number1 = readln().toInt()
+        println("Enter number 2: ")
+        number2 = readln().toInt()
+        println("The quotient is ${number1/number2}")
+    } catch (exception:NumberFormatException) {
+        println("NumberFormatException")
+        println("Wrong Input! Integer Inputs only!")
+    } catch (exception:ArithmeticException) {
+        println("ArithmeticException")
+        println("Wrong Input! Integer Inputs only!")
+    }
+    println("This is a test")
+}
