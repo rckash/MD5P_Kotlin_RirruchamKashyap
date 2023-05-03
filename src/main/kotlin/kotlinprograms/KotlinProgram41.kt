8ce5ac484e88589e5f2c08e11a2e0d7ed8a54d11
RC <rckashyapcpe@gmail.com>
2023-05-03 20:20:27 +0800

    created a program demonstrating lambda function

---
+++ src/main/kotlin/kotlinprograms/KotlinProgram41.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val displayName = { println("Peter") }
+    val addNumbers = { num1:Int, num2:Int -> num1 + num2 }
+
+    displayName()
+    println(addNumbers(5,1))
+}
+
+//fun addNumbers(num1:Int, num2:Int): Int {
+//  return num1 + num2
+//}
+
+//fun displayName() {
+//  println("Peter")
+//}
