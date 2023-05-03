06c3fbf21d91717661fcecf182216552afbc7189
RC <rckashyapcpe@gmail.com>
2023-05-03 20:06:09 +0800

    created a program demonstrating vararg

---
+++ src/main/kotlin/kotlinprograms/KotlinProgram40.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //varargs - variable-length argument list
+    println(total(2,5,10))
+}
+
+fun total(vararg numbers: Int): Int {
+    var total = 0
+    for (num in numbers) {
+        total += num
+    }
+    return total
+}
