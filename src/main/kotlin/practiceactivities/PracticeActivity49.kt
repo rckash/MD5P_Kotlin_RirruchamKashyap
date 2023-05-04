a8263ef6b68202399a3313c9bc94507eac831e58
RC <rckashyapcpe@gmail.com>
2023-05-04 13:39:06 +0800

    converted PracticeActivity37.kt to infix notation

---
+++ src/main/kotlin/practiceactivities/PracticeActivity49.kt
@@ -0,0 +1,28 @@
+package practiceactivities
+
+fun main() {
+    //input
+    println("Enter Length: ")
+    var length = readln().toDouble()
+    println("Enter Width: ")
+    var width = readln().toDouble()
+
+    //function return values storage
+    var area = length calculateArea width
+    var perimeter = length calculatePerimeter width
+
+    //output
+    println("The area of the rectangle is $area")
+    println("The perimeter of the rectangle is $perimeter")
+
+}
+
+infix fun Double.calculateArea(thisDouble: Double): Double {
+    var area = this * thisDouble
+    return area
+}
+
+infix fun Double.calculatePerimeter(thisDouble: Double): Double {
+    var perimeter = this * 2 + thisDouble * 2
+    return perimeter
+}
