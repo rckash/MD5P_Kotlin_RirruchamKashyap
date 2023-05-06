3c69f3a1b5e067ae6035534cb5fa34918ffcb274
RC <rckashyapcpe@gmail.com>
2023-05-07 01:20:17 +0800

    created a program for storing library book borrow data

---
+++ src/main/kotlin/Activity01-I.kt
@@ -0,0 +1,23 @@
+fun main() {
+    //input
+    print("Enter Book Title: ")
+    var book = readln()
+    print("Enter Borrower's Name: ")
+    var borrower = readln()
+    print("Enter Return Deadline: ")
+    var returnDeadline = readln()
+
+    //data storage
+    var borrowSet = mutableSetOf<String>()
+
+    var storeBorrowInfo = { book: String, borrower: String, returnDeadline: String ->       //function for storing borrow data
+        borrowSet.add(book)
+        borrowSet.add(borrower)
+        borrowSet.add(returnDeadline)
+    }
+
+    storeBorrowInfo(book,borrower,returnDeadline)
+
+    //output
+    println(borrowSet)
+}
