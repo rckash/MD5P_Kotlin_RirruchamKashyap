400d9b9fc796f132c0a55c6ac40fbf1586afbebe
RC <rckashyapcpe@gmail.com>
2023-05-09 19:59:50 +0800

    created a class driver file

--- src/main/kotlin/kotlinoop/kotlinoop2/MobilePhoneDriver.kt
+++ src/main/kotlin/kotlinoop/kotlinoop2/MobilePhoneDriver.kt
@@ -1,2 +1,12 @@
-package kotlinoop.kotlinoop2.MobilePhone
+package kotlinoop.kotlinoop2
 
+fun main() {
+    var myPhone = MobilePhone(10)
+    myPhone.checkBalance()
+    myPhone.load(30)
+    myPhone.checkBalance()
+    myPhone.sendTextMessage("Wer na u?")
+    myPhone.sendTextMessage("D2 na me.")
+    myPhone.checkBalance()
+    myPhone.displayLogs()
+}
