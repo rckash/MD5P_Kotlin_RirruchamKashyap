f3d43812bd33c22dee3182cf48d96c862129ee41
RC <rckashyapcpe@gmail.com>
2023-05-09 19:36:57 +0800

    moved packages

--- src/main/kotlin/kotlinoop/kotlinoop1/kotlinOOP1A.kt
+++ src/main/kotlin/kotlinoop/kotlinoop1/kotlinOOP1A.kt
@@ -1,3 +1,16 @@
+//objects - represents something in the real world
+//class - blueprint of an object
+//Structural Programming, Procedural Programming
+//top to bottom
+
+//Person Class
+///////attributes/properties
+//name
+//age
+//////behaviors
+//walk
+//eat
+
 class Person{
     //attributes
     var name: String
--- src/main/kotlin/kotlinoop/kotlinoop1/kotlinOOP1C.kt
+++ src/main/kotlin/kotlinoop/kotlinoop1/kotlinOOP1C.kt
@@ -1,3 +1,16 @@
+//objects - represents something in the real world
+//class - blueprint of an object
+//Structural Programming, Procedural Programming
+//top to bottom
+
+//Person Class
+///////attributes/properties
+//name
+//age
+//////behaviors
+//walk
+//eat
+
 class Person1B(var name: String, var age: Int){
     fun walk() {
         println("A person walks")
