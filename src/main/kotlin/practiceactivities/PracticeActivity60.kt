b729d1111dd80a780c8ad6796bc9e7203d4949c3
RC <rckashyapcpe@gmail.com>
2023-05-10 22:05:52 +0800

    created a program with High Order Function

--- src/main/kotlin/practiceactivities/PracticeActivity60.kt
+++ src/main/kotlin/practiceactivities/PracticeActivity60.kt
@@ -1,25 +1,23 @@
 package practiceactivities
-//fun main() {
-    //Write a function that takes in a list of integers and a higher-order function as arguments.
-    // The higher-order function should take in an integer and return a string.
-    // The function should return a new list containing the strings returned
-    // by the higher-order function for each element in the input list.
-    //For example, given the list [1, 2, 3, 4, 5] and the higher-order function { n -> "Number: $n" },
-    // the function should return the list ["Number: 1", "Number: 2", "Number: 3", "Number: 4", "Number: 5"].
 
-    //variables declaration
-//    var listForHO = MutableList<Int>(1,2,3,4,5)
-//
-//
-//    returnStringHigherOrder(listFunction)
-//
-//}
-//var listFunction = { listForHO: MutableList<Int> ->
-//    var listForHO = MutableList<Int>()
-//    println(MutableList<Int>(listForHO))
-//}
-//
-//
-//fun returnStringHigherOrder (listFunction: (MutableList<Int>) -> Unit) {
-//    println(listFunction(mutableListOf(1,2,3,4,5)))
-//}
+fun main(){
+    //list declaration
+    var myList = mutableListOf<Int>(5, 10, 15, 20, 25, 30)
+
+    //print list function
+    var displayList = { myList: MutableList<Int> -> println(displayList(myList)) }
+
+    myHOFunction { displayList(myList) }
+}
+
+fun myHOFunction(myFunction: () -> Unit){
+    myFunction()
+}
+
+fun displayList(displayList: MutableList<Int>): MutableList<String>{
+    var stringList = mutableListOf<String>()
+    displayList.forEach {
+        stringList.add("Number: $it")
+    }
+    return stringList
+}
